f8cfad88a860bf24c7745ec9695c4efa7ad948d5
Petter Rodhelind <petter.rodhelind@adeprimo.se>
2017-05-24 14:41:08 +0200

    Syntax cleanup
    ...and the use of TRUE/FALSE instead of 1/0 for explicicy

--- src/cmd/acme/exec.c
+++ src/cmd/acme/exec.c
@@ -1374,14 +1374,13 @@ tabexpand(Text *et, Text *_0, Text *argt, int _1, int _2, Rune *arg, int narg)
 		return;
 	w = et->w;
 
-if (w->tabexpand) {
-w->tabexpand = 0;
-warning(nil, "%.*S: Tabexpand OFF\n", w->body.file->nname, w->body.file->name);
-} else {
-w->tabexpand = 1;
-warning(nil, "%.*S: Tabexpand ON\n", w->body.file->nname, w->body.file->name);
-}
-	/* printf("tabexpand: %d\n", w->tabexpand); */
+	if(w->tabexpand){
+		w->tabexpand = FALSE;
+		warning(nil, "%.*S: Tabexpand OFF\n", w->body.file->nname, w->body.file->name);
+	}else{
+		w->tabexpand = TRUE;
+		warning(nil, "%.*S: Tabexpand ON\n", w->body.file->nname, w->body.file->name);
+	}
 }
 
 void
