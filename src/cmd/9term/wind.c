911dcfd317786043c1f3d406207d4aba50891970
Petter Rodhelind <petter.rodhelind@gmail.com>
2017-10-26 11:52:11 +0200

    9term: Use acme colors.

--- src/cmd/9term/wind.c
+++ src/cmd/9term/wind.c
@@ -53,9 +53,12 @@ wmk(Image *i, Mousectl *mc, Channel *ck, Channel *cctl, int scrolling)
 		/* greys are multiples of 0x11111100+0xFF, 14* being palest */
 		grey = allocimage(display, Rect(0,0,1,1), CMAP8, 1, 0xEEEEEEFF);
 		darkgrey = allocimage(display, Rect(0,0,1,1), CMAP8, 1, 0x666666FF);
-		cols[BACK] = display->white;
-		cols[HIGH] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, 0xCCCCCCFF);
-		cols[BORD] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, 0x999999FF);
+		//cols[BACK] = display->white;
+		cols[BACK] = allocimagemix(display, DPaleyellow, DWhite);
+		//cols[HIGH] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, 0xCCCCCCFF);
+		cols[HIGH] = allocimage(display, Rect(0,0,1,1), screen->chan, 1, DDarkyellow);
+		//cols[BORD] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, 0x999999FF);
+		cols[BORD] = allocimage(display, Rect(0,0,1,1), screen->chan, 1, DYellowgreen);
 		cols[TEXT] = display->black;
 		cols[HTEXT] = display->black;
 		titlecol = allocimage(display, Rect(0,0,1,1), CMAP8, 1, DGreygreen);
